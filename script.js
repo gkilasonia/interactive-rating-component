c1e6ac1917c417ef35ec6d3e8e3d92359e27a5fc
gkilasonia <dgiorjio@gmail.com>
2025-07-02 14:52:04 +0400

    finalization

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-const ratingButtons = document.querySelector(".rating-btns");
+const ratingButtons = document.querySelectorAll(".rating-btns");
+// const ratingButton = document.querySelector(".rating-btns");
+const submitBtn = document.getElementById("submit-btn");
+const ratingMainContainer = document.getElementById("rating-main-container");
+const thankYouContainer = document.getElementById("thank-you-container");
+const selectedRatingSpan = document.querySelector(".selected-rating");
 
-ratingButtons.addEventListener("click", console.log("Hello"));
+let selectedRating = 0;
+
+ratingButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    ratingButtons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+    selectedRating = Number(button.textContent);
+    console.log(selectedRatingSpan);
+    console.log(selectedRating);
+  });
+});
+
+submitBtn.addEventListener("click", () => {
+  if (selectedRating > 0) {
+    selectedRatingSpan.textContent = selectedRating;
+    ratingMainContainer.style.display = "none";
+    thankYouContainer.style.display = "flex";
+  } else {
+    alert("Please select a rating number before submitting!");
+  }
+});
+
+// function defaultBackground() {
+//   ratingButtons.forEach((button) => {
+//     button.style.background = "hsl(213, 19%, 18%)";
+//   });
+// }
